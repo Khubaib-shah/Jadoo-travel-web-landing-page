f93dfaab2e6f44c9673c85d213b7951a3f97389e
khubaib shah <bewithbaacha@gmail.com>
2025-06-08 17:52:18 +0500

    Created Hero section left side  component

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -3,34 +3,30 @@ import navLinks from "../utils/navLinks";
 import downArrow from "../assets/left-arrow.png";
 const Header = () => {
   return (
-    <header className="flex justify-between items-center p-4 bg-transparent">
+    <header className="flex justify-between items-center px-4 py-6">
       {/* Logo */}
-      <div className="relative z-1">
+      <div className="relative z-1 w-[100.4px]">
         <img src={Logo} alt="Jadoo Logo" />
-        <span className="bg-amber-500 absolute right-7 top-4  w-4 h-4 -z-1 rounded-full"></span>
-        <span className="bg-amber-500 absolute right-1 top-3 w-4 h-5 -z-1 rounded-full"></span>
+        <span className="bg-amber-500 absolute right-6 top-[14px] size-3 -z-1 rounded-full"></span>
+        <span className="bg-amber-500 absolute right-1 top-3 size-4 -z-1 rounded-full"></span>
       </div>
 
       {/* Links */}
-      <nav className="flex flex-1 justify-end   sansation items-center text-[#212832] mr-24">
-        <ul className="flex md:gap-20 items-center">
-          {Object.entries(navLinks).map((value, index) => {
-            console.log(value);
-
-            return (
-              <li
-                key={index}
-                className="hover:text-[#5a7194]/40 transition-colors font-bold text-lg"
-              >
-                <a href={`#${value[0].toLowerCase()}`}>{value[1]}</a>
-              </li>
-            );
-          })}
+      <nav className="flex flex-1 justify-end sansation items-center text-[#212832] mr-24">
+        <ul className="flex md:gap-20 items-center z-2">
+          {Object.entries(navLinks).map(([key, label], index) => (
+            <li
+              key={index}
+              className="hover:text-[#5a7194]/40 transition-colors font-bold text-lg"
+            >
+              <a href={`#${key}`}>{label}</a>
+            </li>
+          ))}
         </ul>
       </nav>
 
       {/* Buttons */}
-      <div className="flex md:gap-8 items-center font-sans font-bold text-md">
+      <div className="flex md:gap-8 items-center font-sans font-bold text-md z-2">
         <button className="hover:border border-[#212632] px-5 py-2 rounded-md text-[#212632] cursor-pointer">
           Login
         </button>
@@ -39,11 +35,7 @@ const Header = () => {
         </button>
         <button className="flex items-center gap-2 cursor-pointer">
           EN
-          <img
-            src={downArrow}
-            alt="Down Arrow"
-            className="size-4 -rotate-90 "
-          />
+          <img src={downArrow} alt="Down Arrow" className="size-4 -rotate-90" />
         </button>
       </div>
     </header>
--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -1,10 +1,55 @@
-import React from "react";
 import Header from "./Header";
+import DecoreImg from "../assets/Decore.png";
+import decoreImage1 from "../assets/TextDesign.png";
+import Playbutton from "../assets/Polygon1.png";
 
 const Hero = () => {
   return (
-    <div>
+    <div className="relative w-full overflow-hidden">
+      <img
+        src={DecoreImg}
+        alt="Background Image"
+        className="absolute -top-20 -right-20 z-1"
+      />
       <Header />
+
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 border-2 border-[#412a25] rounded-lg bg-white shadow-lg relative z-20">
+        {/* Content */}
+        <div className="flex flex-col mt-20 justify-center items-start  ms-6">
+          <h4 className="font-bold text-[18.39px] text-[#DF6951]">
+            Best Destinations around the world
+          </h4>
+
+          <h1 className="relative text-8xl font-extrabold mt-4 text-[#181D4B] volkhov leading-20 tracking-tighter ">
+            Travel, enjoy <br /> and live a new <br /> and full life
+            <img
+              src={decoreImage1}
+              alt="Text Design"
+              className="absolute top-[68px] -right-[84px] -z-10"
+            />
+          </h1>
+          <p className="text-[#5E6282] text-[14.44px] mt-10 w-[430.33px] poppins">
+            Built Wicket longer admire do barton vanity itself do in it.
+            Preferred to sportsmen it engrossed listening. Park gate sell they
+            west hard for the.
+          </p>
+
+          {/* Button */}
+          <div className="mt-10 flex items-center gap-10">
+            <button className="sansation bg-[#F1A501] text-white  px-6 py-4 rounded-xl cursor-pointer hover:bg-[#F1A501]/80 transition duration-300">
+              Find out more
+            </button>
+            <button className="sansation text-[#686D77] cursor-pointer flex items-center">
+              <span className="bg-[#DF6951] w-12 h-12 rounded-full me-2 justify-center items-center flex">
+                <img src={Playbutton} alt="Play Button" />
+              </span>
+              Play Demo
+            </button>
+          </div>
+        </div>
+        {/* Image */}
+        <div className="z-30">akdhkas</div>
+      </div>
     </div>
   );
 };
